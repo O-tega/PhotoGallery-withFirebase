fe0fe66eb357871c4cc15b23fc2ba8e8127675d1
O-tega <tega.myview@gmail.com>
2022-09-19 15:01:03 +0100

    another method to open and close modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
 
-const Modal =({ selectedImg, showModal, setShowModal })=>{
+const Modal =({ selectedImg, setSelectedImg })=>{
+
+    const handleClick = (e)=>{
+        if(e.target.classList.contains('backdrop')){
+            setSelectedImg(null)
+        }
+    }
 
     return (
 			<div>
 				{/*<img src={selectedImg} alt="Enlarged modal" />*/}
-				{/*---------------------------------------------------------------------------------*/}
-				{showModal ? (
 					<div
-						className='flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-50 transition delay-700 '
-						onClick={() => {
-							setShowModal(false);
-						}}>
+						className='backdrop flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-black bg-opacity-50 transition delay-700 '
+						onClick={handleClick}>
 						<div>
 							<img
 								src={selectedImg}
@@ -21,8 +23,6 @@ const Modal =({ selectedImg, showModal, setShowModal })=>{
 							/>
 						</div>
 					</div>
-				) : null}
-
 				{/*---------------------------------------------------------------------------------*/}
 			</div>
 		);}
