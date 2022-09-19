733aa27b014b78b46c6a58f239fb62f22999ffa1
O-tega <tega.myview@gmail.com>
2022-09-19 15:49:47 +0100

    added animation to project

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore'
+import { motion } from 'framer-motion'
 
 
 const ImageGrid = ({setSelectedImg, selectedImg}) =>{
@@ -7,10 +8,13 @@ const ImageGrid = ({setSelectedImg, selectedImg}) =>{
     const { docs } = useFirestore('images');
 
     const Ddocs = docs.map(({url, id})=>(
-            <div key={id}
-            onClick = {()=>{setSelectedImg(url)} }>
-                <img src={url} alt="" className='rounded object-cover w-72 h-72'/>
-            </div>
+            <motion.div key={id}
+            onClick = {()=>{setSelectedImg(url)} }
+            layout
+            className='opacity-80 hover:opacity-100'
+            >
+                <motion.img src={url} alt="" className='rounded object-cover w-72 h-72 ' initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 1}}/>
+            </motion.div>
         ))
 
     return (
